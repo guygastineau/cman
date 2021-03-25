8b0d4483801ad49b21bc045f7be4fb7bf03fa184
Guy Gastineau <strings.stringsandstrings@gmail.com>
2021-03-25 15:15:48 -0400

    Remove static from the embeded bytes array types

--- src/embed_unity_c.h
+++ src/embed_unity_c.h
@@ -1,6 +1,6 @@
 #ifndef EMBED_UNITY_C_H
 #define EMBED_UNITY_C_H
 
-const static unsigned char unity_c_bytes[974];
+const unsigned char unity_c_bytes[974];
 
 #endif // EMBED_UNITY_C_H
--- src/embed_unity_h.h
+++ src/embed_unity_h.h
@@ -1,6 +1,6 @@
 #ifndef EMBED_UNITY_H_H
 #define EMBED_UNITY_H_H
 
-const static unsigned char unity_h_bytes[951];
+const unsigned char unity_h_bytes[951];
 
 #endif // EMBED_UNITY_H_H
